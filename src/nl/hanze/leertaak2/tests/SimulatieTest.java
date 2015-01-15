b01271a994b984ff34663ef6723522ec4d58296a
Roelof Kallenkoot <roelof000@gmail.com>
2015-01-15 13:30:32 +0100

    Voegt 3 test methodes toe.
    De public methodes: simulateOneStep, runLongSimulation en simulate
    worden hier getest.

--- src/nl/hanze/leertaak2/tests/SimulatieTest.java
+++ src/nl/hanze/leertaak2/tests/SimulatieTest.java
@@ -1,19 +1,43 @@
 package nl.hanze.leertaak2.tests;
 
+import nl.hanze.leertaak2.*;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
-public class SimulatieTest {
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Random;
 
+public class SimulatieTest {
+	
+	// we hebben animals nodig
+	// een field waar de animals in kunnen
+	// een simulator view waar we het in laten zien???
+	
+	private Simulator sim;
+	private static final int SIZE = 30;
+	private static final int STEPS = 37;
+	
 	@Before
 	public void setUp() throws Exception {
+		sim = new Simulator(SIZE,SIZE);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_simulateOneStep() {
+		sim.simulateOneStep();
+	}
+	
+	@Test
+	public void test_runLongSimulation(){
+		sim.runLongSimulation();
+	}
+	
+	@Test
+	public void test_simulateMultipleSteps(){
+		sim.simulate(STEPS);
 	}
 
 }
